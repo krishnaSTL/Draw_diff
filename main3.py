a32ec0e7fbc7ec35ae730dbedcdb48d6755ba120
adityaSTL <111113768+adityaSTL@users.noreply.github.com>
2022-11-25 10:53:28 +0530

    nhai data xl

--- main3.py
+++ main3.py
@@ -6,7 +6,8 @@ import matplotlib.image as mpimg
 global y_divider1,y_divider2,y_road1,y_road2,y_roadside1,y_roadside2,x,len,y
 y,x,len=0,2,1
 
-
+def get_df():
+    return pd.read_excel(r'Untitled 1.xls')
 
 def get_divider(d=3):
     global y_divider1,y_divider2,y
@@ -17,8 +18,12 @@ def get_divider(d=3):
 
 def get_road(d1=9,d2=9,d3=8.5):
     global y_road1,y_road2
-    y_road1=y_divider1+d1
-    y_road2=y_divider2-d2
+    try:
+        y_road1=y_divider1+d1
+        y_road2=y_divider2-d2
+    except:
+        y_road1=d1
+        y_road2=-d2
     plt.hlines(y_road1,x,x+len,linestyles='solid',colors='black',lw=1)
     plt.hlines(y_road2,x,x+len,linestyles='solid',colors='black',lw=1)
 
@@ -48,13 +53,21 @@ def outer_box(d=3):
 def get_objects_roadside(d=1):
     yl=y_roadside1+d
     yr=y_roadside2-d-d/2    
-    df=pd.read_excel(r'Untitled 1.xls')
+    df=get_df()
     #print(df.index)
     #print(df,len(df.index))
     for i in df.index:
         print(float(df['Chainage'][i]),d,df['RHS'][i])
         plt.text(float(df['Chainage'][i]),yr,df['RHS'][i],fontsize = 9,color='Black')
         plt.text(float(df['Chainage'][i]),yl,df['LHS'][i],fontsize = 9,color='Black')
+
+
+def get_chainage(d=3):
+    df=get_df()
+    yr=y_roadside2-d
+
+    for i in df.index:
+        plt.text(float(df['Chainage'][i]),yr,df['Chainage'][i],fontsize = 10,color='Black',rotation=90)
 
 
 def draw_sld():
@@ -78,11 +91,13 @@ def draw_sld():
 fig=plt.figure(figsize=(30,8))   
 ax = plt.gca()
 ax.set_xlim(x-0.09, x+len+0.09)
-get_divider()
-get_road()
-get_roadside()
-outer_box(7)
+plt.axis('off')
+# get_divider()
+# get_road()
+#get_roadside()
+# outer_box(7)
 get_objects_roadside()
+# get_chainage()
 
 #plt.hlines(3,0,1,linestyles='solid',colors='black',lw=1)
 plt.show()
